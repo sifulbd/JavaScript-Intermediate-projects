2de48dae750d95a32cf39e44723ef099e3efa66a
sifulbd <sifulsmm@gmail.com>
2020-04-01 14:24:36 +0600

    inital commit

---
+++ todo.js
@@ -0,0 +1,60 @@
+
+var theTodo = {
+    todoList: [],
+    displayTodo: function(){
+        if (this.todoList.length === 0 ) {
+            console.log("Your todo list is empty! ");
+        } else {
+            for(var i = 0; i < this.todoList.length; i++ ) {
+                if (this.todoList[i].complete === true) {
+                    console.log('✅', this.todoList[i].todoText);
+                }else {
+                    console.log(' ', this.todoList[i].todoText);
+                }
+            }
+        }            
+    },
+
+    toggleAll: function() {
+            var alltodos = this.todoList.length;
+            var todotsCompleted = 0;
+
+        for(var i = 0; i < alltodos; i++ ) {
+            if(this.todoList[i].complete === true) {
+                todotsCompleted++;
+            }
+        }
+        if(todotsCompleted === alltodos) {
+            for (var i = 0; i < alltodos; i++) {            
+                this.todoList[i].complete = false;
+            }
+        }
+        else {
+            for (var i = 0; i < alltodos; i++) {            
+                this.todoList[i].complete = true;
+            }
+        }
+    },
+
+    addItem: function (item) {
+        this.todoList.push({
+            todoText: item,
+            complete: false
+        }); 
+        this.displayTodo();
+    },
+    changeItem: function (position, todoText) { 
+        // this.todoList[position] = changeName;
+        this.todoList[position].todoText = todoText;
+        this.displayTodo();
+    },
+    deleteItem: function (todoText){
+        this.todoList.splice(todoText, 1);
+        this.displayTodo();
+    },
+    todoToggle: function(position) {
+        var todo = this.todoList[position];
+        todo.complete = !todo.complete;
+        this.displayTodo();
+    }
+}
